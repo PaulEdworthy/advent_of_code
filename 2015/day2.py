298b427e4f7a19cc25b884b80662d98c88bb1cfe
PaulEdworthy <pauledworthy@gmail.com>
2022-10-14 06:13:00 -0400

    complete day 1 and 2

--- 2015/day2.py
+++ 2015/day2.py
@@ -2,12 +2,20 @@
 
 with open('day2_volume.txt') as f:
     total = 0
+    total_extra = 0
     for line in f:
 
-        l, w, h = map(int, line.split('x'))
-        wrap = 2 * (l * w) + (w * h) + (h * l)
-        extra = min(l * w, w * h, h * l)
+        l, w, h = sorted(map(int, line.split('x')))
+        wrap = (2 * l * w) + (2 * w * h) + (2 * h * l)
+        extra = (l * w)
         paper = wrap + extra
         total += paper
 
         print(f"Total {total}")
+
+# Part 2
+        bow = l * w * h
+        ribbon = 2 * (l + w)
+        total_extra += (bow + ribbon)
+
+        print(f"Ribbon {total_extra}")
