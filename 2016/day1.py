1c3b7a3650977d9499c7c83bd1d1643dbd04f013
PaulEdworthy <dev-edu.goawn@simplelogin.com>
2022-11-19 12:21:29 -0500

    add day1 input file

---
+++ 2016/day1.py
@@ -0,0 +1,12 @@
+# starting point is facing North. Follow instructions to end.
+# how many blocks from the start are you?
+# R2, R2, R2 isn't 6 blocks from the start, it's 2
+
+x = y = 0
+direction = 'north'
+
+with open('day1.txt') as f:
+    buffer = f.readline()
+    sp = buffer.split(', ')
+    for d in sp:
+        
