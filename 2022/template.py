450bb401c5c8ee3972d975f45a17afe9775289d7
PaulEdworthy <dev-edu.goawn@simplelogin.com>
2022-12-05 07:26:40 -0500

    modify template

--- 2022/template.py
+++ 2022/template.py
@@ -1,7 +1,10 @@
 import aocd
 
-date =
+# date =
 data = aocd.get_data(year=2022, day=date).split("\n")  # TODO: change day
 
 for line in data:
     print(line)
+
+# ~/.config/aocd either change token or
+# save txt file in folder
