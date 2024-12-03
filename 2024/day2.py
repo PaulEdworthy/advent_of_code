7e09316e16986d1fcd429ba79c13b6e4024c5036
PaulEdworthy <pauledworthy@gmail.com>
2024-12-03 18:18:48 -0500

    Created loader file to import data directly

--- 2024/day2.py
+++ 2024/day2.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from load_aoc_data import get_aoc_input
 
-data = get_aoc_input(2024, 1)
+data = get_aoc_input(2024, 2)
 
 # test the input just grabbing the first and last lines
 print(f"First line {data[0]}, and the last line {data[-1]}")
---
+++ load_aoc_data.py
@@ -0,0 +1,21 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+def get_aoc_input(year, day):
+    session_cookie = {'session': os.getenv('AOC_SESSION_COOKIE')}
+    
+    url = f'https://adventofcode.com/{year}/day/{day}/input'
+    response = requests.get(url, cookies=session_cookie)
+    return response.text.strip().split('\n') # strip whitespace and split into lines
+
+    if response.status_code == 200:
+        print("Data downloaded successfully")
+        return data
+
+    else:
+        print(f"Failed to download input for Day {day}")
+        return None
