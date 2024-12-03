f94d0cfe6b95889b6660399a63accd1582e2ff29
PaulEdworthy <pauledworthy@gmail.com>
2024-12-03 18:31:10 -0500

    Completed day 1

--- 2024/day1.py
+++ 2024/day1.py
@@ -6,5 +6,23 @@ from load_aoc_data import get_aoc_input
 
 data = get_aoc_input(2024, 1)
 
-# test the input just grabbing the first and last lines
-print(f"First line {data[0]}, and the last line {data[-1]}")
+left, right = [], []
+total = 0
+
+for line in data:
+    value = line.split()
+    left.append(int(value[0]))
+    right.append(int(value[1]))
+
+left.sort()
+right.sort()
+
+# Part 1
+for i in range(len(left)):
+    total += abs(left[i] - right[i])
+
+# Part 2
+for i in left:
+    total += i * right.count(i)
+
+print(total)
