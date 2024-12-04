6fff5a2c8cc33aaa5cb86267374e05e39e55da10
PaulEdworthy <pauledworthy@gmail.com>
2024-12-03 21:31:13 -0500

    Complete day 2 part 1

--- 2024/day2.py
+++ 2024/day2.py
@@ -6,5 +6,21 @@ from load_aoc_data import get_aoc_input
 
 data = get_aoc_input(2024, 2)
 
-# test the input just grabbing the first and last lines
-print(f"First line {data[0]}, and the last line {data[-1]}")
+safe = 0
+
+def is_increasing(input):
+    return all(input[i] < input[i + 1] and input[i + 1] - input[i] <= 3 for i in range(len(input) - 1))
+
+
+def is_decreasing(input):
+    return all(input[i] > input[i + 1] and input[i] - input[i + 1] <= 3 for i in range(len(input) - 1))
+
+
+for line in data:
+    value = [int(i) for i in line.split()] # convert a list of strings to a list of ints
+    
+    if is_increasing(value) or is_decreasing(value):
+        safe += 1
+
+
+print(safe)
