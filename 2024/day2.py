611d4360adc3b7b268ac1b93315849dac7338ed4
PaulEdworthy <pauledworthy@gmail.com>
2024-12-05 15:53:23 -0500

    Complete day 2

--- 2024/day2.py
+++ 2024/day2.py
@@ -16,11 +16,22 @@ def is_decreasing(input):
     return all(input[i] > input[i + 1] and input[i] - input[i + 1] <= 3 for i in range(len(input) - 1))
 
 
+def check_dampener(input):
+    for i in range(len(input)):
+        new_input = input[:i] + input[i+1:]
+        if is_increasing(new_input) or is_decreasing(new_input):
+            return True
+    return False
+
+
 for line in data:
     value = [int(i) for i in line.split()] # convert a list of strings to a list of ints
     
     if is_increasing(value) or is_decreasing(value):
         safe += 1
-
+    
+    else:
+        if check_dampener(value):
+            safe += 1
 
 print(safe)
