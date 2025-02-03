deb9a9678c24ec817c50eb0d3cd62e52d8b8d661
PaulEdworthy <pauledworthy@gmail.com>
2025-02-03 16:17:42 -0500

    complete day 1 in C++

--- 2015/C++/d01.cpp
+++ 2015/C++/d01.cpp
@@ -7,6 +7,8 @@ int main() {
     char ch;
     std::ifstream infile(url);
     std::string data;
+
+    int part1(std::string data);
     
     if (!infile) {
         std::cerr << "File not found\n";
@@ -14,8 +16,28 @@ int main() {
     } else {
         std::getline(infile, data);
     }
-    std::cout << data << "\n";
-
     infile.close();
+    std::cout << part1(data) << "\n";;
     return 0;
+}
+
+int part1(std::string data) {
+    int floor = 0;
+    int basement = 0;
+    bool isFalse = false;
+    for (int i = 0; i < data.size(); i++) {
+        if (data[i] == '(') {
+            floor++;
+        } else if (data[i] == ')') {
+            floor--;
+        } else {
+            continue;
+        }
+        // Part 2
+        if (floor == -1 && !isFalse) {
+            basement = i;
+            isFalse = true; // setting the flag to false will ensure the loop won't run after the 1st time
+        }
+    }
+    return floor, basement;
 }
